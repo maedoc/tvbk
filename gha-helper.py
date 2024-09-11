3fc24b2cdf00cbf1211dc23277add8d6514b2f73
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2024-09-11 22:30:28 +0200

    fix ci

--- gha-helper.py
+++ gha-helper.py
@@ -4,6 +4,7 @@ import glob
 import shutil
 import os
 import platform
+import importlib.util
 
 matrix_os, = sys.argv[1:]
 
@@ -19,14 +20,7 @@ whl_name, = glob.glob('tvbk-kernels-wheel/tvb_kernels*.whl')
 log(f'installing {whl_name}')
 os.system(f'{sys.executable} -m pip install {whl_name}')
 
-import site
-tvbk_name = None
-for pkg in site.getsitepackages():
-    fs = glob.glob(os.path.join(pkg, 'tvbk.py'))
-    if fs:
-        tvbk_name, = fs
-        break
-
+tvbk_name = importlib.util.find_spec('tvbk').origin
 log(f'found {tvbk_name}, installing tvbk.so')
 so_dst = os.path.join(os.path.dirname(tvbk_name), 'libtvbk.so')
 shutil.copy(so_name, so_dst)
