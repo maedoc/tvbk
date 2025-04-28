98b5e6decca9ed89882d44b7699e2a73b2b87f7c
marmaduke woodman <marmaduke.woodman@univ-amu.fr>
2025-04-28 13:45:07 +0200

    feat: add foo.c

---
+++ foo.c
@@ -0,0 +1,35 @@
+#include <math.h>
+
+#define INLINE __attribute((always_inline)) inline
+
+INLINE static void dfun(int n, float *dx, float *x)
+{
+    #pragma omp simd
+    for (int i=0; i<n; i++)
+    {
+        dx[i] = -x[i] + 0.1*sinf(x[i]);
+    }
+}
+
+typedef struct heun_work {
+    const int n;
+    const float dt;
+    float *dx1, *dx2, *xi;
+} heun_work_t;
+
+INLINE static void heun_step(heun_work_t w, float *x) {
+    dfun(w.n, w.dx1, x);
+    #pragma omp simd
+    for (int i=0; i<w.n; i++)
+        w.xi[i] = x[i] + w.dt*w.dx1[i];
+    dfun(w.n, w.dx2, w.xi);
+    #pragma omp simd
+    for (int i=0; i<w.n; i++)
+        x[i] += w.dt*0.5*(w.dx1[i] + w.dx2[i]);
+}
+
+void tvbk_model_stepn(int n, float *x) {
+    float dx1[128], dx2[128], xi[128];
+    heun_work_t w = {.n = n, .dt=0.1, .dx1=dx1, .dx2=dx2, .xi=xi};
+    heun_step(w, x);
+}
